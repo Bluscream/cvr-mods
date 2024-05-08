a19251c98af49192f7b1028969d732836cea26e2
Bluscream <Bluscream@users.noreply.github.com>
2024-05-08 17:21:55 +0200

    fixup + prep PropSpawner

---
+++ PropSpawner/Main.cs
@@ -0,0 +1,24 @@
+﻿using ABI_RC.Core.Savior;
+using ABI_RC.Systems.GameEventSystem;
+using UnityEngine.SceneManagement;
+using MelonLoader;
+
+namespace Bluscream.PropSpawner;
+
+public class PropSpawner : MelonMod {
+    public override void OnInitializeMelon() {
+        ModConfig.InitializeMelonPrefs();
+
+        CVRGameEventSystem.Instance.OnConnected.AddListener(instance => {
+            if (!ModConfig.EnableMod.Value) return;
+            Task.Factory.StartNew(() => CommonMethods.SpawnProps());
+        });
+    }
+
+    internal static class CommonMethods {
+        internal static void SpawnProps(string? worldId = null) {
+            if (!ModConfig.EnableMod.Value) return;
+            worldId ??= MetaPort.Instance.CurrentWorldId;
+        }
+    }
+}
--- PropSpawner/ModConfig.cs
+++ PropSpawner/ModConfig.cs
@@ -1,7 +1,7 @@
-﻿using Bluscream.PublicSafety.Properties;
+﻿using Bluscream.PropSpawner.Properties;
 using MelonLoader;
 
-namespace Bluscream.PublicSafety;
+namespace Bluscream.PropSpawner;
 
 public static class ModConfig {
     private static MelonPreferences_Category _melonCategory;
@@ -18,12 +18,22 @@ public static class ModConfig {
 
     public static void InitializeMelonPrefs() {
         _melonCategory = MelonPreferences.CreateCategory(AssemblyInfoParams.Name);
-        EnableMod = _melonCategory.CreateEntry("Enable Prop Spawner", true,
+        EnableMod = _melonCategory.CreateEntry("Enable Public Safety", true,
             description: "The mod will do nothing while disabled");
 
         EnableURLWhitelist = _melonCategory.CreateEntry("Enable URL Whitelist in publics", true,
             description: "Will automatically switch on URL Whitelist when you join a public instance");
         DisableURLWhitelist = _melonCategory.CreateEntry("Disable URL Whitelist in non-publics", false,
             description: "Will automatically switch off URL Whitelist when you join anything other than a public instance");
+
+        EnableAdvancedSafety = _melonCategory.CreateEntry("Enable Advanced Safety in publics", true,
+            description: "Will automatically switch on Advanced Safety when you join a public instance");
+        DisableAdvancedSafety = _melonCategory.CreateEntry("Disable Advanced Safety in non-publics", false,
+            description: "Will automatically switch off Advanced Safety when you join anything other than a public instance");
+
+        DisableRichPresence = _melonCategory.CreateEntry("Disable Rich Presence in non-publics", true,
+            description: "Will automatically switch off Rich Presence (Steam+Discord) when you join anything other than a public instance");
+        EnableRichPresence = _melonCategory.CreateEntry("Enable Rich Presence in publics", false,
+            description: "Will automatically switch on Rich Presence (Steam+Discord) when you join a public instance");
     }
 }
