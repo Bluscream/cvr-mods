2d32ff67474a02ba449824729ba348cff37edc37
Bluscream <Bluscream@users.noreply.github.com>
2024-06-29 03:51:07 +0200

    a

--- MoreChatNotifications/Properties/AssemblyInfo.cs
+++ MoreChatNotifications/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using MelonLoader;
 namespace Bluscream.MoreChatNotifications.Properties;
 internal static class AssemblyInfoParams {
     public const string Name = "MoreChatNotifications";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Author = "Bluscream";
 }
